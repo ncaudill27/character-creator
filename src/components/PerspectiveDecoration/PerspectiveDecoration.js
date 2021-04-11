0f9ade22039a93e53a059996d3976916d323341f
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-04-11 10:37:04 -0400

    resort to z-index for perpectiveDecoration

---
+++ src/components/PerspectiveDecoration/PerspectiveDecoration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PerspectiveDecoration.module.css';
+
+const PerspectiveDecoration = () => (
+  <div className={styles.root} />
+)
+
+export default PerspectiveDecoration
